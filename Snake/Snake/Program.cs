67a3ecab858e48c7ee222cea6e8896e5a8e44338
vasdud <oprell@yandex.ru>
2016-08-01 13:07:23 +0300

    Add function Drow(int x, int y, char symb)

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,8 +9,25 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HW !");
+            int x1 = 1;
+            int y1 = 3;
+            char c1 = '*';
+
+            Draw(x1, y1, c1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char c2 = '#';
+
+            Draw(x2, y2, c2);
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write( symb );
         }
     }
 }
