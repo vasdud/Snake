03840c84b6fbd4a979a7025fed8c82e442df8033
vasdud <oprell@yandex.ru>
2016-08-01 13:46:31 +0300

    Add class Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sumb;
+
+        public void Draw(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,25 +9,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char c1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sumb = '*';
+            p1.Draw(p1.x, p1.y, p1.sumb);
 
-            Draw(x1, y1, c1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char c2 = '#';
-
-            Draw(x2, y2, c2);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sumb = '#';
+            p2.Draw(p2.x, p2.y, p2.sumb);
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char symb)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write( symb );
         }
     }
 }
